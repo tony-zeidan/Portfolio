7524a90d130ca4e26fc260b1907c8c16d0e023ff
Tony Abou-Zeidan <tony.azp25@gmail.com>
2023-10-31 14:41:23 -0400

    snowfall changes

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ import GamesPage from "./components/Games/GamesPage";
 import ProjectsPage from "./components/ProjectPages/ProjectsPage";
 import EducationPage from "./components/EducationPages/EducationPage";
 import Navbar from "./components/Navbar";
+import Snowfall from 'react-snowfall'
 
 function App() {
     return (
         <div>
             <BrowserRouter>
+                <div className="fixed w-screen h-screen top-0">
+                <Snowfall
+                snowflakeCount={25}/>
+                    ;
+                </div>
                 <Navbar/>
                 <Routes>
                     <Route index element={<HomePage/>}/>
