2efb233d782ec4bb8481a13a5af3c343dd7f8c78
Tony Abou-Zeidan <tony.azp25@gmail.com>
2026-09-10 16:44:31 -0400

    education page update

--- src/components/EducationPages/EducationPage.jsx
+++ src/components/EducationPages/EducationPage.jsx
@@ -1,42 +1,46 @@
-import React, {useEffect} from 'react'
-import Navbar from "../Navbar";
+import React from 'react'
+import Reveal from "../Reveal";
 
 const EducationPage = () => {
     return (
         <div>
-            <Navbar/>
-            <div id='projects' className='w-full h-full text-gray-300 flex-nowrap bg-[#181818]'>
+            <div id='education' className='w-full h-full text-gray-300 flex-nowrap bg-[#181818]'>
 
                 <div className='max-w-[1000px] mx-auto p-4 w-full h-full'>
-                    <div id="geohexviz" className='mt-36'>
-                        <div className='pb-8 w-full'>
-                            <p className='text-5xl text-start font-bold border-b-4 text-gray-300 border-cyan-500'>
+                    <div className='mt-36'>
+                        <Reveal className='pb-8 w-full'>
+                            <p className='text-4xl sm:text-5xl font-bold text-white'>
                                 Education
                             </p>
-                        </div>
-                        <div>
+                            <div className='accent-rule h-[3px] w-16 rounded-full mt-4' />
+                        </Reveal>
+                        <Reveal delay={60}>
                             <p className='text-3xl text-start text-gray-400'>Carleton University</p>
-                        </div>
-                        <div className='mb-5 mt-5'>
-                            <p>Program: <em>Software Engineering</em></p>
-                            <p>Degree: <em>Bachelors of Engineering (B.Eng)</em></p>
-                            <p>CGPA: <em>11.30 / 12</em></p>
+                        </Reveal>
+                        <Reveal delay={120} className='mb-5 mt-5'>
+                            <p>Program: <em>Software Engineering (SYSC)</em></p>
+                            <p>Degree: <em>Bachelor of Engineering (B.Eng)</em></p>
+                            <p>Years: <em>2019 to 2023</em></p>
+                            <p>CGPA: <em>11.05 / 12</em></p>
+                            <p>Award: <em>Entrance Scholarship</em></p>
                             <p className='mt-2'>I studied for a total of 4 years and did 1 year of co-op at Carleton University. </p>
-                        
-<div class="flex flex-col text-gray-300">
-  <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-    <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-      <div class="overflow-hidden">
-        <table class="min-w-full">
-          <thead class="border-b">
+                        </Reveal>
+                        <div className='mb-5 mt-5'>
+
+<div className="flex flex-col text-gray-300">
+  <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+    <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+      <div className="overflow-hidden">
+        <table className="min-w-full">
+          <thead className="border-b">
             <tr>
-              <th scope="col" class="text-sm font-bold px-6 py-4 text-left">
+              <th scope="col" className="text-sm font-bold px-6 py-4 text-left">
                 Course Code
               </th>
-              <th scope="col" class="text-sm font-bold px-6 py-4 text-left">
+              <th scope="col" className="text-sm font-bold px-6 py-4 text-left">
                 Course Name
               </th>
-              <th scope="col" class="text-sm font-bold px-6 py-4 text-left">
+              <th scope="col" className="text-sm font-bold px-6 py-4 text-left">
                 Letter Grade
               </th>
             </tr>
@@ -47,8 +51,8 @@ const EducationPage = () => {
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium font-bold"></td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium font-bold"></td>
             </tr>
-            <tr className="border-b" colspan='3'>
-                <td className="px-6 py-4 whitespace-nowrap text-sm font-bold">Level One Courses:</td>
+            <tr className="border-b">
+                <td colSpan={3} className="px-6 py-4 whitespace-nowrap text-sm font-bold">Level One Courses:</td>
             </tr>
             <tr>
               <td className="text-sm font-light px-6 py-4 whitespace-nowrap">
@@ -182,8 +186,8 @@ const EducationPage = () => {
                 A
               </td>
             </tr>
-            <tr className="border-b border-t" colspan='3'>
-                <td className="px-6 py-4 whitespace-nowrap text-sm font-bold">Level Two Courses:</td>
+            <tr className="border-b border-t">
+                <td colSpan={3} className="px-6 py-4 whitespace-nowrap text-sm font-bold">Level Two Courses:</td>
             </tr>
             <tr>
               <td className="text-sm font-light px-6 py-4 whitespace-nowrap">
@@ -284,8 +288,8 @@ const EducationPage = () => {
                 A+
               </td>
             </tr>
-            <tr className="border-b border-t" colspan='3'>
-                <td className="px-6 py-4 whitespace-nowrap text-sm font-bold">Level Three Courses:</td>
+            <tr className="border-b border-t">
+                <td colSpan={3} className="px-6 py-4 whitespace-nowrap text-sm font-bold">Level Three Courses:</td>
             </tr>
             <tr>
               <td className="text-sm font-light px-6 py-4 whitespace-nowrap">
@@ -375,8 +379,8 @@ const EducationPage = () => {
                 A+
               </td>
             </tr>
-            <tr className="border-b border-t" colspan='3'>
-                <td className="px-6 py-4 whitespace-nowrap text-sm font-bold">Level Four Courses:</td>
+            <tr className="border-b border-t">
+                <td colSpan={3} className="px-6 py-4 whitespace-nowrap text-sm font-bold">Level Four Courses:</td>
             </tr>
             <tr>
               <td className="text-sm font-light px-6 py-4 whitespace-nowrap">
@@ -419,7 +423,7 @@ const EducationPage = () => {
               Discrete Simulation/Modeling
               </td>
               <td className="text-sm font-light px-6 py-4 whitespace-nowrap">
-                In Progress
+                A- / A / A+
               </td>
             </tr>
             <tr>
@@ -452,7 +456,7 @@ const EducationPage = () => {
               Software Architecture and Design
               </td>
               <td className="text-sm font-light px-6 py-4 whitespace-nowrap">
-                In Progress
+                A- / A / A+
               </td>
             </tr>
             <tr>
@@ -463,7 +467,7 @@ const EducationPage = () => {
               Introduction to Machine Learning
               </td>
               <td className="text-sm font-light px-6 py-4 whitespace-nowrap">
-                In Progress
+                A- / A / A+
               </td>
             </tr>
             <tr>
@@ -474,7 +478,7 @@ const EducationPage = () => {
               Introduction to Web Development
               </td>
               <td className="text-sm font-light px-6 py-4 whitespace-nowrap">
-                In Progress
+                A- / A / A+
               </td>
             </tr>
             <tr>
@@ -485,7 +489,7 @@ const EducationPage = () => {
               Software Laboratory
               </td>
               <td className="text-sm font-light px-6 py-4 whitespace-nowrap">
-                In Progress
+                A- / A / A+
               </td>
             </tr>
             <tr className='border-b'>
@@ -505,7 +509,53 @@ const EducationPage = () => {
     </div>
   </div>
 </div>
+                            <p className='mt-4 text-sm text-gray-500'>
+                                Final-year courses show the grade range rather than the exact letter. Each was
+                                an A-, A or A+; I no longer have the individual letters on hand.
+                            </p>
                         </div>
+
+                        <Reveal className='pb-8 w-full mt-16'>
+                            <p className='text-4xl sm:text-5xl font-bold text-white'>
+                                Courses
+                            </p>
+                            <div className='accent-rule h-[3px] w-16 rounded-full mt-4' />
+                        </Reveal>
+                        <Reveal delay={60} className='mb-5'>
+                            <p>Angular, The Complete Guide. Udemy, 56 hours, 2025.</p>
+                        </Reveal>
+
+                        <Reveal className='pb-8 w-full mt-16'>
+                            <p className='text-4xl sm:text-5xl font-bold text-white'>
+                                Publications
+                            </p>
+                            <div className='accent-rule h-[3px] w-16 rounded-full mt-4' />
+                        </Reveal>
+                        <Reveal delay={60} className='mb-24'>
+                            <p>
+                                Abou Zeidan, T. M., &amp; Rempel, M. "GeoHexViz: A Python package for
+                                visualizing hexagonally binned geospatial data." <em>Journal of Open
+                                Source Software</em>, 2023.
+                            </p>
+                            <ul className='mt-3 flex flex-col gap-2'>
+                                <li className='px-0 cursor-auto'>
+                                    <a
+                                        className='text-gray-400 hover:text-cyan-500 transition-colors duration-200 break-all'
+                                        href='https://joss.theoj.org/papers/c051df96dac973486cc312452575e804'
+                                    >
+                                        joss.theoj.org/papers/c051df96dac973486cc312452575e804
+                                    </a>
+                                </li>
+                                <li className='px-0 cursor-auto'>
+                                    <a
+                                        className='text-gray-400 hover:text-cyan-500 transition-colors duration-200 break-all'
+                                        href='https://cradpdf.drdc-rddc.gc.ca/PDFS/unc381/p814091_A1b.pdf'
+                                    >
+                                        cradpdf.drdc-rddc.gc.ca/PDFS/unc381/p814091_A1b.pdf
+                                    </a>
+                                </li>
+                            </ul>
+                        </Reveal>
                     </div>
                 </div>
             </div>
